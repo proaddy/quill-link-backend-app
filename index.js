1223efa4882bf35930cd1071d45fd7036857b532
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2024-10-26 21:11:35 +0530

    first get api

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!!!');
+const express = require('express');
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res)=>{
+    res.send("Hello from node api at /");
+});
+
+app.get('/trash', (req, res)=>{
+    res.send("Hello from trash page node API");
+});
